bc8a606163706195c7d9dfc32c48d21d1c68d633
bankmarqs <danilojmarqs@gmail.com>
2024-09-16 12:17:27 -0400

    Abrindo portas TCP

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(4000);
+  await app.listen(3002);
 }
 bootstrap();
