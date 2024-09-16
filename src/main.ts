421d15f8f99fffd6a0d17b26738b02cf40fc20a6
bankmarqs <danilojmarqs@gmail.com>
2024-09-16 13:30:40 -0400

    Atualizando code

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(8080);
+  await app.listen(4000);
 }
 bootstrap();
