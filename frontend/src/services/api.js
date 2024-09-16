7494a76c085918100562de1fdb9264eea9111e72
bankmarqs <danilojmarqs@gmail.com>
2024-09-16 11:55:59 -0400

    Consertando IP da VM

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://179.191.232.25/:4000'; 
+const API_BASE_URL = 'http://179.191.232.25:4000'; 
 
 const api = axios.create({
   baseURL: API_BASE_URL,
